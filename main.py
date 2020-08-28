e25226d9fa5105753a62b25e045d7fca4a221cc1
tomas <38633386+Tomasroma64@users.noreply.github.com>
2020-08-28 23:54:11 +0200

    💡 Comments

--- main.py
+++ main.py
@@ -1,30 +1,36 @@
-
-
 import lxml.html
 import random 
 import time
 from selenium import webdriver
 
+# Make sure that the URL says "pageNumber=1"  (you can also change it manually)
+# Most probably digital.ucas.com
 url = input("url: ")
-driver = webdriver.Chrome()
-courses = ""
-todosleep = True
-fname = str(random.randint(0,1000)) + ".txt"
-for i in range(10000):
-    #print(str(i))
 
+# Install and include chrome driver in the same folder
+driver = webdriver.Chrome()
+
+courses = ""
+
+todosleep = True
+
+# To have a unique output file name
+fname = str(random.randint(0,1000)) + ".txt"
+
+# You might have to stop the loop manualy, there isnt a check for the end of the list
+for i in range(10000):
+
+    # Next page (havent tested for number 0 lol)
     driver.get(url.replace("pageNumber=1", "pageNumber=" + str(i)))
 
-    tree = lxml.html.fromstring(driver.page_source)
-
-    #results = driver.find_element_by_xpath('course-title')   
-
+    # Gives you a chance to change any settigns before the process begins (helps you change from different grading systems)
     if(todosleep):
         time.sleep(5)
         todosleep = False
 
     ok = True
     while ok:
+        # Details of different elements
         courseName = driver.find_elements_by_class_name('course-title') 
         courseUni = driver.find_elements_by_class_name('provider') 
         coursePoints = driver.find_elements_by_class_name('ucas-points') 
@@ -37,13 +43,12 @@ for i in range(10000):
                 courses += courseName[i].text + " ; " + coursePoints[i].text[9:] + " ; " + courseUni[i].text + "\n"
             except:
                 ok = True
-                print("error")
+                print("error") # Sometimes stuff goes wrong
 
+        # Append on every page to not loose all data in the case of an error
         with open(fname,"a+") as f:
             f.write(courses)
         
         courses = ""
 
-
-
-print(courses)
+print("Done")
